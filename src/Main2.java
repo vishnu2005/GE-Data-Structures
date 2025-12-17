aec988986b10736dd65c592cfbcda54dc66c6d9c
Vishnu Priya <vishnupriyavivek13@gmail.com>
2025-12-17 13:11:19 +0530

    Added UC-2 for HashTable Assignment

--- src/Main2.java
+++ src/Main2.java
@@ -17,6 +17,29 @@ public class Main2{
 
         System.out.println("UC-1 :");
         list.display();
+        System.out.println("\n ----------------------------------------- \n");
+
+        System.out.println("UC-2 :");
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Enter paragraph:");
+        String paragraph = s.nextLine().toLowerCase();
+
+        // remove punctuation
+        // [^a-z] - Match any character that is NOT a lowercase letter and NOT a space
+        paragraph = paragraph.replaceAll("[^a-z ]", "");
+
+        String[] words_in_para = paragraph.split("\\s+");
+
+        MyHashTable table = new MyHashTable();
+
+        for (String word_in_para : words_in_para) {
+            table.add(word_in_para);
+        }
+
+        System.out.println("Word Frequencies: (UC-2)");
+        table.display();
+
 
     }
 }
